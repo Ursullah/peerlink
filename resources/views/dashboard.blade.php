7b15925ff3efb6a6bd51f65edabd29ed64fb4289
Singason Simon <swangui@kabarak.ac.ke>
2025-10-17 14:19:36 +0300

    notifications - transactions status

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,13 @@
 
     <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (session('transaction_failed'))
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                    <strong class="font-bold">Transaction Failed!</strong>
+                    <span class="block sm:inline">{{ session('transaction_failed') }}</span>
+                </div>
+            @endif
+            
             @if (session('success'))
                 <div 
                     x-data="{ show: true }" 
